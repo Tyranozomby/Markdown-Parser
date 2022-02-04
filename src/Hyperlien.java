1e5d19604c0fc9954db045817ba8f739fa281104
Tyranozomby <eliott.rogeaux@gmail.com>
2022-02-04 20:02:05 +0100

    Ajout pdf

--- src/Hyperlien.java
+++ src/Hyperlien.java
@@ -8,12 +8,27 @@ public class Hyperlien extends StyleMD {
         if (url == null || url.trim().equals("")) {
             this.url = "http://localhost";
         } else {
-            this.url = url;
+            this.url = url.trim();
         }
     }
 
     @Override
     public String formater(String texte) {
-        return "[" + texte + "] (\"" + url + "\")";
+        return "[" + texte + "] (" + url + ")";
     }
+
+    public String getUrl(){
+        return url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+//        super.equals();
+//        if (!(autreStyle instanceof StyleMD))
+//            return false;
+//
+//        StyleMD autre = (StyleMD) autreStyle;
+//        return this.disposition == autre.disposition;
+    }
+
 }
